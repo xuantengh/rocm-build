37735bd64ecbdd575b58e15c61dfbab6797d9abb
Huangxt57 <Huangxt57@users.noreply.github.com>
2024-01-22 04:21:43 +0000

    Sync changes

--- driver.sh
+++ driver.sh
@@ -6,17 +6,28 @@ if [ "$EUID" -ne 0 ]
 fi
 
 set -e
-wget http://repo.radeon.com/amdgpu-install/latest/ubuntu/jammy/amdgpu-install_5.5.50500-1_all.deb -P /root/downloads
+wget https://repo.radeon.com/amdgpu-install/latest/ubuntu/jammy/amdgpu-install_6.0.60000-1_all.deb -P /root/downloads
 apt install -y /root/downloads/amdgpu-install*.deb
 apt update
 which amdgpu-install
 # Backup
 amdgpu-install -h > /root/amdgpu-install.option.txt
 amdgpu-install --list-usecase >> /root/amdgpu-install.option.txt
-amdgpu-install -y --accept-eula --no-32 --usecase=dkms --dryrun > /root/amdgpu-install.commands.txt
-amdgpu-install -y --accept-eula --no-32 --usecase=dkms
-# Check installation
-lsmod | grep amd
+amdgpu-install -y --accept-eula --no-32 --usecase=dkms --dryrun > /root/amdgpu-install.commands.sh
+
+# amdgpu-install -y --accept-eula --no-32 --usecase=dkms
+bash /root/amdgpu-install.commands.sh
 
 apt update
 apt install libstdc++-12-dev
+
+# Optional: fetch `amdgpu-dkms` .deb src from AMD website, and build from source
+wget https://repo.radeon.com/amdgpu/6.0/ubuntu/pool/main/a/amdgpu-dkms/amdgpu-dkms_6.3.6.60000-1697589.22.04_all.deb -P /root/downloads
+dkpg-deb -R /root/downloads/amdgpu-dkms_6.3.6.60000-1697589.22.04_all.deb \
+  /tmp/amdgpu-dkms
+wget https://repo.radeon.com/amdgpu/6.0/ubuntu/pool/main/a/amdgpu-dkms/amdgpu-dkms-firmware_6.3.6.60000-1697589.22.04_all.deb -P /root/downloads
+dkpg-deb -R /root/downloads/amdgpu-dkms-firmware_6.3.6.60000-1697589.22.04_all.deb /tmp/amdgpu-dkms-firmware
+
+# Check installation
+lsmod | grep amd
+
